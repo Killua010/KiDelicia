34bb8fc9b7a053def0a4c6fab2a60541abdf791a
Daniel Dias <daniel160598@hotmail.com>
2018-12-19 08:29:04 -0200

    Projeto iniciado

---
+++ back/src/main/java/br/com/kidelicia/KiDeliciaApplication.java
@@ -0,0 +1,14 @@
+package br.com.kidelicia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiDeliciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiDeliciaApplication.class, args);
+	}
+
+}
+
